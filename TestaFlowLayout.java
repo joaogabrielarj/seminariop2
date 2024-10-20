d1e7de48f8c14e6020500b18751d5bc828c4963c
joaogabrielarj <joaogabrielaraujo30@gmail.com>
2024-10-20 15:21:36 -0300

    primeiro commit

---
+++ TestaFlowLayout.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TestaFlowLayout {
+    public static void main(String[] args) {
+
+        FlowLayoutFrame fl = new FlowLayoutFrame();
+        fl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fl.setSize(600, 75);	//CONFIGURA O TAMANHO DA TELA
+        fl.setVisible(true);	//CONFIGURA A VISIBILIDADE
+
+
+    }
+}
